230513f28ad42ece78ff55b69f5d9cdfe76845ba
clark.chang <clark.chang@ctitv.com.tw>
2025-12-02 08:44:06 +0800

    fix: Add log

--- scripts/cwa_weather.sh
+++ scripts/cwa_weather.sh
@@ -37,7 +37,7 @@ log() {
 
     [[ $current_level -gt $level ]] && return 0
 
-    printf "%(%F %T)T [%5s] %s\n" "${levels["$level"]}" "$msg" >> "$log_file"
+    printf "%(%F %T)T [%5s] %s\n" -1 "${levels["$level"]}" "$msg" >> "$log_file"
 }
 
 log_debug() {
@@ -54,12 +54,12 @@ check_dependencies() {
 
     if ! command -v curl &> /dev/null ; then
         code=1
-        printf "No curl installed\n" >&2
+        log_error "No curl installed"
     fi
 
     if ! command -v jq &> /dev/null ; then
         code=1
-        printf "No jq installed\n" >&2
+        log_error "No jq installed"
     fi
 
     return "$code"
@@ -84,7 +84,10 @@ get_latest_cache() {
     local cache
 
     for file in "${dir}/${location}-"*.json; do
-        [[ -f "$file" ]] && cache="$file"
+        if [[ -f "$file" ]]; then
+            log_debug "Found cache: $file"
+            cache="$file"
+        fi
     done
 
     printf "%s" "$cache"
@@ -97,11 +100,23 @@ is_valid_cache() {
     local file_name
     file_name=${cache##*/}
 
-    [[ -z "$cache" || ! "$file_name" =~ .+-[0-9]{10}\.json || ! -f "$cache" ]] && return 1
+    if [[ -z "$cache" || ! "$file_name" =~ .+-[0-9]{10}\.json ]]; then
+        log_debug "Invalid cache file name: $cache"
+        return 1
+    fi
+
+    if [[ ! -f "$cache" ]]; then
+        log_debug "Cache file does not exist: $cache"
+        return 1
+    fi
 
     # yyyyMMddHH
     local file_date_hour
+
+    # Remove extension
     file_date_hour=${file_name%%.*}
+
+    # Remove location(e.g. 臺北市-)
     file_date_hour=${file_date_hour##*-}
 
     local file_date
@@ -109,6 +124,8 @@ is_valid_cache() {
 
     local file_hour
     file_hour="${file_date_hour: -2:2}"
+
+    # Remove leading zero
     file_hour=${file_hour#0}
 
     local date
@@ -117,7 +134,12 @@ is_valid_cache() {
     local hour
     hour="$(printf "%(%k)T")"
 
-    [[ "$date" > "$file_date" || $((hour - file_hour)) -ge 3 ]] && return 1 || return 0
+    if [[ "$date" > "$file_date" || $((hour - file_hour)) -ge 3 ]]; then
+        log_debug "Cache expired: $cache. Cache time: ${file_date} ${file_hour}h. Current time: ${date} ${hour}h."
+        return 1
+    else
+        return 0
+    fi
 }
 
 call_api() {
@@ -131,13 +153,15 @@ call_api() {
     cache="$(get_latest_cache "$location_name")"
 
     if [[ -n "$cache" ]] && ! is_valid_cache "$cache"; then
+        log_debug "Delete invalid cache: $cache"
+
         rm "$cache"
         cache=""
     fi
 
     if [[ -z "$cache" ]]; then
         if [[ -z "$api_key" ]]; then
-            printf "No CWA weather api key\n" >&2
+            log_error "No CWA weather api key"
             return 1
         fi
 
@@ -156,7 +180,7 @@ call_api() {
             --output "$cache" \
             "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"; then
 
-            printf "curl failed with %d exit code\n" "$?" >&2
+            log_error "curl failed with $? exit code"
             return 1
         fi
     fi
@@ -835,7 +859,7 @@ get_temperature() {
     elif [[ "$type" == "min" ]]; then
         name=MinT
     else
-        printf "Invalid type: %s\n" "$2" >&2
+        log_error "Invalid type: $2"
         return 1
     fi
 
@@ -847,7 +871,7 @@ check_dependencies || exit 1
 response="$(call_api)" || exit 1
 
 if ! check_api_status "$response"; then
-    printf "API failed\n" >&2
+    log_error "API failed"
     exit 1
 fi
 
