6242006bef96ae65cbab8f2a58611426095db876
shootingstar <jinhung.c@gmail.com>
2026-09-17 15:54:54 +0800

    fix: Add percent symbol

--- scripts/cpu_info.sh
+++ scripts/cpu_info.sh
@@ -9,7 +9,7 @@ get_percent()
 {
   case $(uname -s) in
     Linux)
-      percent=$(top -bn2 -d 0.01 | grep "[C]pu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id, *\([0-9.]*\)%* wa.*/\1 \2/" | awk '{usage = 100 - $1 - $2; if (usage < 0.005) usage = 0; printf usage"%"}')
+      percent=$(top -bn2 -d 0.01 | grep "[C]pu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id, *\([0-9.]*\)%* wa.*/\1 \2/" | awk '{usage = 100 - $1 - $2; if (usage < 0.005) usage = 0; printf "%s%%", usage}')
       normalize_percent_len $percent
       ;;
 
